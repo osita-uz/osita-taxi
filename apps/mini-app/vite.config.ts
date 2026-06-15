2b57348bfab2f321c1f3cb62fe5a1db19505c7df
0605AbMu <0605AbMu@gmail.com>
2026-06-15 18:29:13 +0500

    feat: update allowedHosts in Vite config for mini app

--- apps/mini-app/vite.config.ts
+++ apps/mini-app/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: "0.0.0.0",
-    allowedHosts: ["taxi.jprq.live"],
+    allowedHosts: [".jprq.live", ".ngrok-free.app"],
     proxy: {
       "/api": {
         target: "http://192.168.0.211:3000",
